2bf5153fab287edebd4d499893e000f2697eff5f
tuanneli <zhuravlev99pic@gmail.com>
2022-11-20 01:20:34 +0300

    commit

--- server/middlewares/errorMiddleware.js
+++
@@ -1,8 +0,0 @@
-import ApiError from "../errors/ApiError.js";
-
-export default function errorMiddleware(err, req, res, next) {
-  if (err instanceof ApiError) {
-    return res.status(err.status).json({message: err.message, errors: err.error});
-  }
-  return res.status(500).json({message: "Внутрення ошибка сервера"});
-}
